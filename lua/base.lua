7a8e5fde7e3b0ed9b494cdcc57c4a450501b87d2
Tyler Morgan <tylermorgan86@gmail.com>
2022-12-20 12:26:47 -0800

    not sure, but something changed

--- lua/base.lua
+++ lua/base.lua
@@ -73,7 +73,7 @@ ts.setup {
   ensure_installed = {
     "tsx",
     "toml",
-    "fish",
+    "bash",
     "php",
     "json",
     "yaml",
@@ -108,29 +108,16 @@ autopairs.setup({
 require('gitsigns').setup {}
 
 -- Null-ls
-local augroup = vim.api.nvim_create_augroup("LspFormatting", {})
-require("null-ls").setup({
-    sources = {
-      require("null-ls").builtins.eslint.code_actions.eslint,
-      require("null-ls").builtins.lua.formatting.lua
-    }
-})
+local status, null_ls = pcall(require, "null-ls")
+if (not status) then return end
 
-require("null-ls").setup({
-    -- you can reuse a shared lspconfig on_attach callback here
-    on_attach = function(client, bufnr)
-        if client.supports_method("textDocument/formatting") then
-            vim.api.nvim_clear_autocmds({ group = augroup, buffer = bufnr })
-            vim.api.nvim_create_autocmd("BufWritePre", {
-                group = augroup,
-                buffer = bufnr,
-                callback = function()
-                    -- on 0.8, you should use vim.lsp.buf.format({ bufnr = bufnr }) instead
-                    vim.lsp.buf.formatting_sync()
-                end,
-            })
-        end
-    end,
+null_ls.setup({
+  sources = {
+    null_ls.builtins.diagnostics.eslint_d.with({
+      diagnostics_format = '[eslint] #{m}\n(#{c})'
+    }),
+    null_ls.builtins.diagnostics.zsh
+  }
 })
 
 -- Prettier
